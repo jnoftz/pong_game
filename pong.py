0d857aa1a97b77bbfc58a9fc723cccf25510f5db
Jerry Noftz <noftz@underworldsystems.com>
2021-10-29 13:30:53 -0400

    Update ball and paddle logic

--- pong.py
+++ pong.py
@@ -1,12 +1,98 @@
 import turtle
 
 wn = turtle.Screen()
-wn.title("Test")
+wn.title("Pong")
 wn.bgcolor("black")
-wn.setup(width=800, height=800)
+wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#Paddle Left 
+paddle_left = turtle.Turtle()
+paddle_left.speed(0)
+paddle_left.shape("square")
+paddle_left.color("white")
+paddle_left.shapesize(stretch_wid=5, stretch_len=1)
+paddle_left.penup()
+paddle_left.goto(-350, 0)
+
+#Paddle Right
+paddle_right = turtle.Turtle()
+paddle_right.speed(0)
+paddle_right.shape("square")
+paddle_right.color("white")
+paddle_right.shapesize(stretch_wid=5, stretch_len=1)
+paddle_right.penup()
+paddle_right.goto(+350, 0)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+ball.dx = .1
+ball.dy = .1
+
+#Function
+def paddle_left_up():
+    y = paddle_left.ycor()
+    y += 20
+    paddle_left.sety(y)
+
+def paddle_left_down():
+    y = paddle_left.ycor()
+    y -= 20
+    paddle_left.sety(y)
+
+def paddle_right_up():
+    y = paddle_right.ycor()
+    y += 20
+    paddle_right.sety(y)
+
+def paddle_right_down():
+    y = paddle_right.ycor()
+    y -= 20
+    paddle_right.sety(y)    
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_left_up, "w")
+wn.onkeypress(paddle_left_down, "s")
+wn.onkeypress(paddle_right_up, "Up")
+wn.onkeypress(paddle_right_down, "Down")
 
 #Main game loop
 while True:
     wn.update()
+
+    #Move Ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border Check
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    #Paddle Collision
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_right.ycor() + 40 and ball.ycor() > paddle_right.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1  
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_left.ycor() + 40 and ball.ycor() > paddle_left.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1  
